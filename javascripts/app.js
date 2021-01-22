3270c7cc6481cf210f54759c565fb825c437a40e
Pablo Aquino <pablo311098@gmail.com>
2021-01-22 02:01:57 -0300

    agrege el calculo de los años y la tabla de deudas con sus interacciones: agregarlas y eliminarlas

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,35 @@
-// let usuario = new User(5000, 1000, 6)
 
-// console.log(usuario);
+const salary = document.getElementById("salary");
+const savings= document.getElementById("savings");
+const interest = document.getElementById("interest");
+const yearsDisplay = document.getElementById("years");
+const buttonCalculate = document.getElementById("calculate");
+
+if(localStorage.key("usuario")){
+    salary.value = JSON.parse(localStorage.getItem("usuario")).salary;
+    savings.value = JSON.parse(localStorage.getItem("usuario")).savings;
+    interest.value = JSON.parse(localStorage.getItem("usuario")).interest;
+    var localUser = new User(parseFloat(salary.value), parseFloat(savings.value), parseFloat(interest.value));
+    yearsDisplay.textContent = localUser.calculateYearsToRetire();
+}else{
+    var localUser = new User(0, 0, 0);
+}
+
+
+buttonCalculate.addEventListener('click', buttonClicked);
+
+function buttonClicked(){
+    
+    yearsDisplay.textContent = localUser.calculateYearsToRetire();
+    
+    localUser.salary = salary.value
+    localUser.savings = savings.value
+    localUser.interest = interest.value
+
+    localStorage.setItem("usuario", JSON.stringify(localUser));
+    console.log(localStorage.getItem("usuario"));
+    
+}
+
+localUser.buildDebtsSection();
+
--- javascripts/user.js
+++ javascripts/user.js
@@ -1,31 +1,128 @@
 class User {
-    salary = 0;
-    savings = 0;
-    interest = 6;
 
-    constructor(salary, savings, interest){
+    constructor(salary, savings, interest) {
         this.salary = salary;
         this.savings = savings;
         this.interest = interest;
     }
 
-    returnYears(){
+    calculateYearsToRetire() {
+        let costoVida = salary.value - savings.value;
+        let fondo = (costoVida * 12) / (interest.value / 100); //fondo total ahorrado necesario para retirarse
+
+        let total = 0; //total del fondo desde que se comienza a ahorrar
+        let años = 0;
+
+        if (fondo == Infinity) {
+            años = "∞"
+        } else {
+            while (total < fondo) {
+                total = (total + savings.value * 12) * (1 + interest.value / 100);
+                años += 1
+            }
+        }
+
+        return años;
+    }
+
+    addInvesment() {
 
     }
 
-    addSavings(){
+    removeInvesment() {
 
     }
 
-    removeSavings(){
+    buildDebtsSection() {
+        const debtText = document.getElementById("debtText");
+        const debtAmount = document.getElementById("debtAmount");
+        const debtInterest = document.getElementById("debtInterest");
+        const buttonAddDebt = document.getElementById("debtButton");
 
+        const tableDebt = document.getElementById("tableDebt");
+
+        buttonAddDebt.addEventListener('click', function () {
+
+            //ingresamos los datos al json que los guardará
+            data.push(
+                {
+                    "id": data.length,
+                    "name": debtText.value,
+                    "amount": debtAmount.value,
+                    "interest": debtInterest.value,
+                    "enabled" : true,
+                }
+            )
+            
+            //construimos la tabla
+            localUser.loadTableDebt();
+
+            //reemplazamos el texto del contenedor de deuda que agrega la informacion    
+            debtText.value = "";
+            debtAmount.value = "";
+            debtInterest.value = "";
+
+        });
     }
 
-    addDebt(){
+    loadTableDebt(){
+        //borramos todos las deudas que existen en la tabla
+        while (tableDebt.firstChild) {
+            tableDebt.removeChild(tableDebt.lastChild);
+        }
 
+        //cargamos nuevamente todos los datos que se encuentran en el json
+        //si esta deshabilitado, no los cargo (no se como eliminarlo del array sin causar problemas con los id's)
+        data.forEach(function (element) {
+            if(element.enabled){
+                let oneMoreDebt = localUser.addDebt(element.id, element.name, element.amount, element.interest);
+            tableDebt.appendChild(oneMoreDebt);
+            }
+        });
+        //selecciono los botones - y por cada uno agrego una escucha, para removerlos si se presiona
+        const buttonSubstract = document.querySelectorAll(".debtButtonSubstract");
+        buttonSubstract.forEach(function(element){
+            element.addEventListener("click", localUser.removeDebt);
+        })
     }
 
-    removeDebt(){
+    addDebt(id, text, amount, interest) {
+        //construyo cada elemento
+        const div = document.createElement("div");
+        const textElement = document.createElement("input");
+        const amountElement = document.createElement("input");
+        const interestElement = document.createElement("input");
+        const buttonElement = document.createElement("button");
+
+        //le agrego una clase a cada elemento
+        div.classList.add("formToAddDebt");
+        textElement.classList.add("debtText");
+        amountElement.classList.add("debtAmount");
+        interestElement.classList.add("debtInterest")
+        buttonElement.classList.add("debtButtonSubstract");
+
+        //le agrego un contenido al elemento
+        textElement.value = text;
+        amountElement.value = amount;
+        interestElement.value = interest;
+        buttonElement.textContent = "−";
+        buttonElement.setAttribute("data-id", id);
+        //buttonElement.addEventListener('click', this.removeDebt)
+
+
+        //introduzco cada elemento dentro del primer div
+        div.appendChild(textElement);
+        div.appendChild(amountElement);
+        div.appendChild(interestElement);
+        div.appendChild(buttonElement);
+
+        //debo retornar el div para ingresarlo en el contenedor
+        return div;
+    }
+
+    removeDebt(event) {
         
+        data[event.target.dataset.id].enabled = false;
+        localUser.loadTableDebt();
     }
 }
